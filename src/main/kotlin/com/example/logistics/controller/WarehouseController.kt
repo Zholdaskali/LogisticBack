204d44d4f7e865d2775166033a3230d741a38d2e
Zholdaskali <zholdaskalierkebulan@gmail.com>
2024-06-03 19:21:47 +0500

    Init full backend

--- src/main/kotlin/com/example/logistics/controller/WarehouseController.kt
+++ src/main/kotlin/com/example/logistics/controller/WarehouseController.kt
@@ -3,8 +3,12 @@ package com.example.logistics.controller
 import com.example.logistics.model.Warehouse
 import com.example.logistics.repository.WarehouseRepository
 import com.example.logistics.response.MessagedResponse
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,5 +21,13 @@ class WarehouseController(
     @GetMapping
     fun getAll(): MessagedResponse<List<Warehouse>> {
         return warehouseRepository.findAll().let(MessagedResponse.Companion::of)
+    }
+
+    @PostMapping("/add")
+    fun createWarehouse(
+        @RequestBody warehouse: Warehouse
+    ): ResponseEntity<MessagedResponse<Warehouse>> {
+        warehouseRepository.save(warehouse)
+        return ResponseEntity.ok(MessagedResponse.of(warehouse))
     }
 }
